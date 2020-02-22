dd053058f87eca3573b87c51bb866717326debf1
ayumu.mochizuki <ayumu839.a@gmail.com>
2020-02-23 00:16:26 +0900

    ボードの描画処理を二重ループに変えた

--- src/board.js
+++ src/board.js
@@ -14,23 +14,25 @@ class Board extends React.Component {
   }
 
   render() {
+    const board = [];
+
+    for (let col = 0; col < 3; col++) {
+      const rows = [];
+      for (let row = 0; row < 3; row++) {
+        rows.push(
+          this.renderSquare((3 * col) + row)
+        )
+      }
+
+      board.push(
+        <div className="board-row">
+          {rows}
+        </div>
+      );
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {board}
       </div>
     );
   }
